783483c49604ab8570c5617e3f017144ac92b643
Boby Kurniawan <bobykurniawan@bankmandiri.co.id>
2022-12-21 14:21:13 +0700

    [FIXING] CHANGE UPDATE HANDLER WITH DEFAULTIFBLANK

--- src/main/java/com/mandiri/shippoapps/service/UpdateShippoService.java
+++ src/main/java/com/mandiri/shippoapps/service/UpdateShippoService.java
@@ -4,6 +4,7 @@ import com.mandiri.shippoapps.common.Message;
 import com.mandiri.shippoapps.model.entity.Shippo;
 import com.mandiri.shippoapps.repository.ShippoRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.var;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -21,21 +22,17 @@ public class UpdateShippoService {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, message);
         }
 
-        if (StringUtils.isBlank(updateStore.getName())){
-            updateStore.setName(shippo.getName());
-        }
-        if (StringUtils.isBlank(updateStore.getAddress())) {
-            updateStore.setAddress(shippo.getAddress());
-        }
-        if (StringUtils.isBlank(updateStore.getPhone())){
-            updateStore.setPhone(shippo.getPhone());
-        }
-        if (StringUtils.isBlank(updateStore.getPermitNumber())){
-            updateStore.setPermitNumber(shippo.getPermitNumber());
-        }
-        if (StringUtils.isBlank(updateStore.getTaxNumber())){
-            updateStore.setTaxNumber(shippo.getTaxNumber());
-        }
+        var name = StringUtils.defaultIfBlank(shippo.getName(), updateStore.getName());
+        var address = StringUtils.defaultIfBlank(shippo.getAddress(), updateStore.getAddress());
+        var phone = StringUtils.defaultIfBlank(shippo.getPhone(), updateStore.getPhone());
+        var permitNumber = StringUtils.defaultIfBlank(shippo.getPermitNumber(), updateStore.getPermitNumber());
+        var taxNumber = StringUtils.defaultIfBlank(shippo.getTaxNumber(), updateStore.getTaxNumber());
+
+        System.out.println(name);
+        System.out.println(address);
+        System.out.println(phone);
+        System.out.println(permitNumber);
+        System.out.println(taxNumber);
 
         shippoRepository.save(updateStore);
     }
